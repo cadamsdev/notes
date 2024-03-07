a7e9c868c35eb3d65fdc5a36fd105ca9462fc198
cadamsdev <cadamsdev@outlook.com>
2024-03-06 23:25:28 -0500

    chore: added blocknote dep

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
+import "@blocknote/react/style.css";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+export default function App() {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
+
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} />;
 }
-
-export default App
