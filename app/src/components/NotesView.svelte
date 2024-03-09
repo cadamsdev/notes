9e4c99a0250f8888ccfc066cf083ae138d23d3b8
cadamsdev <cadamsdev@outlook.com>
2024-03-09 15:34:05 -0500

    chore: added iconfiy icons, moved create note to NotesView

--- app/src/components/NotesView.svelte
+++ app/src/components/NotesView.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 	import { notes, selectedNote, type Note } from '../store';
 
 	function selectNote(note: Note): void {
 		selectedNote.update(() => note);
 	}
+
+	async function createNote(): Promise<void> {
+		const lastID = await window.electron.createNote('A title', 'A note');
+		console.log(lastID);
+
+		notes.update((items) => {
+			items.push({ id: lastID, title: 'A title', content: 'A note' });
+			return items;
+		});
+	}
 </script>
 
-<div class=" bg-slate-200">
-	<div class="p-6">
-		<h2 class="text-lg font-bold mb-6">Some folder</h2>
+<div class="bg-slate-200">
+	<div class="p-4">
+		<div class="flex justify-end">
+			<button on:click={createNote} class="bg-slate-200 hover:bg-slate-300 p-2 rounded mb-4"
+				><Icon icon="fa-solid:plus" /></button
+			>
+		</div>
 		<input class="p-2 rounded mb-4" placeholder="Search..." />
-	</div>
 
-	{#each $notes as note}
-		<button class="block w-full text-left p-4 bg-slate-300 hover:bg-slate-400" on:click={() => selectNote(note)}>
+			{#each $notes as note}
+		<button
+			class="block rounded w-full text-left p-4 bg-slate-300 hover:bg-slate-400"
+			on:click={() => selectNote(note)}
+		>
 			{note.id}
 			{note.title}
 		</button>
 	{/each}
+	</div>
 </div>
--- app/src/components/Sidebar.svelte
+++ app/src/components/Sidebar.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import { notes } from "../store";
-
 	const tags = ['java', 'python', 'c++'];
-
-	async function createNote(): Promise<void> {
-    const lastID = await window.electron.createNote('A title', 'A note');
-    console.log(lastID);
-
-		notes.update((items) => {
-			items.push({ id: lastID, title: 'A title', content: 'A note' });
-			return items;
-		});
-	}
 </script>
 
 <div class="p-6 bg-slate-800">
-	<button on:click={createNote} class="bg-slate-200 hover:bg-slate-300 p-2 rounded mb-4"
-		>+ Create note</button
-	>
-
 	<div class="text-white">
 		<div class="uppercase mb-4">Tags</div>
 		{#each tags as tag}
