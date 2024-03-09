130c327ea0e11d63d130356a4f53c690312601d2
cadamsdev <cadamsdev@outlook.com>
2024-03-08 23:07:15 -0500

    chore: Migrated NotesView and NoteView to svelte

---
+++ app/src/components/NotesView.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="p-6 bg-slate-200">
+	<h2 class="text-lg font-bold mb-6">Some folder</h2>
+	<input class="p-2 rounded" placeholder="Search..." />
+</div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-  import Sidebar from '../components/Sidebar.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+	import NotesView from '../components/NotesView.svelte';
+	import NoteView from '../components/NoteView.svelte';
 </script>
 
-<Sidebar />
+<div class="flex h-full">
+	<Sidebar />
+
+	<NotesView />
+
+	<div class="flex-grow p-4">
+		<NoteView />
+	</div>
+</div>
