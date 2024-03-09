8b9ae57efd663f6ca8a6119e1b105cf20a0d755c
cadamsdev <cadamsdev@outlook.com>
2024-03-09 18:25:44 -0500

    chore: setup EditorJS

--- app/src/components/NoteView.svelte
+++ app/src/components/NoteView.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
 	import { selectedNote } from '../store';
+	import { onMount } from 'svelte';
+
+	let editorRef: any = null;
+
+	onMount(async () => {
+		const Header: any = (await import('@editorjs/header')).default;
+		const EditorJS = await import('@editorjs/editorjs');
+		const editor = new EditorJS.default({
+			holder: editorRef,
+			placeholder: 'Type / for commands',
+			inlineToolbar: true,
+			tools: {
+				header: { class: Header, inlineToolbar: true }
+			}
+		});
+	});
 </script>
 
 {#if $selectedNote?.title}
@@ -9,3 +25,5 @@
 {#if $selectedNote?.content}
 	<div>{$selectedNote?.content}</div>
 {/if}
+
+<div bind:this={editorRef} id="editor"></div>
