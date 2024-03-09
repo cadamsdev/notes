2c6cd7ed28174e78436747c8013dd9eb27bfa4b7
cadamsdev <cadamsdev@outlook.com>
2024-03-09 15:43:11 -0500

    chore: added icon for tags

--- app/src/components/Sidebar.svelte
+++ app/src/components/Sidebar.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+
 	const tags = ['java', 'python', 'c++'];
 </script>
 
 <div class="p-6 bg-slate-800">
 	<div class="text-white">
-		<div class="uppercase mb-4">Tags</div>
+		<div class="mb-4 text-sm flex items-center gap-2">
+			<Icon icon="fa-solid:tags" />
+			Tags
+		</div>
 		{#each tags as tag}
 			<div class="pl-4">
-				{tag}<span class="text-gray-400">&nbsp;1</span>
+				{tag}<span class="text-gray-400 text-sm">&nbsp;1</span>
 			</div>
 		{/each}
 	</div>
