dee72ba5fff894df2de0ba6cdd3ee41c3dd0a9de
cadamsdev <cadamsdev@outlook.com>
2024-03-16 22:59:29 -0400

    chore: create tags table

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -3,3 +3,10 @@ create table notes(
   title text not null,
   content text
 );
+
+create table tags(
+  id integer primary key autoincrement not null,
+  note_id integer not null,
+  name text not null,
+  FOREIGN KEY (note_id) REFERENCES notes(id)
+);
