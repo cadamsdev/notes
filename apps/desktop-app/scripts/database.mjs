e1145105c7a27e87f725787107d9f555dbfa7515
cadamsdev <cadamsdev@outlook.com>
2024-04-16 22:22:08 -0400

    chore: move to npm workspaces, moved database file to home dir, dist electron app

--- apps/desktop-app/scripts/database.mjs
+++ apps/desktop-app/scripts/database.mjs
@@ -1,9 +1,24 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import os from 'os';
+import path from 'path';
+import { closeSync, existsSync, mkdirSync, openSync } from 'fs';
 
 async function connect() {
+  const baseDir = path.join(os.homedir(), '.blocknotes');
+  if (!existsSync(baseDir)) {
+    mkdirSync(baseDir);
+  }
+
+  const dbFilePath = path.join(baseDir, 'database.db');
+  if (!existsSync(dbFilePath)) {
+    closeSync(openSync(dbFilePath, 'w'));
+    // TODO seed the database file
+  }
+
+
   return open({
-    filename: 'tmp/database.db',
+    filename: dbFilePath,
     driver: sqlite3.Database,
   });
 }
