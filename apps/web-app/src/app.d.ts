eaf3cdc4e651a7500623be42b6a106cdc25c8cd5
cadamsdev <cadamsdev@outlook.com>
2024-05-07 23:42:51 -0400

    chore: remove electron type defs

--- apps/web-app/src/app.d.ts
+++ apps/web-app/src/app.d.ts
@@ -1,9 +1,4 @@
 // See https://kit.svelte.dev/docs/types#app
-
-import type { Tag } from "./interfaces/Tag";
-import type { TagRecord } from "./interfaces/TagRecord";
-import type { Note } from "./store";
-
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -12,13 +7,6 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
-	}
-
-	interface Window {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		electron: {
-			editTag: (tag: TagRecord) => void;
-		};
 	}
 }
 
