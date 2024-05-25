39cb2a220cc8fab9319231abf55b6cd115dc8053
cadamsdev <cadamsdev@outlook.com>
2024-05-25 15:43:43 -0400

    wip

--- apps/web-app/src/components/ConfirmationDialog.svelte
+++ apps/web-app/src/components/ConfirmationDialog.svelte
@@ -22,14 +22,14 @@
 <Modal {showModal} on:closeModal={handleCloseModal}>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
-	<div class="rounded bg-bg border border-bg-secondary p-3 shadow-lg inline-block z-20 min-w-[300px]">
-		<div class="flex items-center justify-between mb-8">
-			<h2 class="text-xl text-text-primary font-bold text-center">Confirm Action</h2>
-			<button on:click={handleCloseModal} class="hover:text-gray-600 text-text-secondary">
+	<div class="dialog">
+		<div class="dialog-top">
+			<h2 class="heading">Confirm Action</h2>
+			<button on:click={handleCloseModal} class="close-btn">
 				<Icon icon="fa-solid:times" width="24" height="24" />
 			</button>
 		</div>
-		<div class="min-w-[250px] max-w-[325px] text-text-secondary">
+		<div class="dialog-content">
 			{#if description}
 				<div class="mb-8">
 					{description}
@@ -43,3 +43,45 @@
 		</div>
 	</div>
 </Modal>
+
+<style>
+	.dialog {
+		display: inline-block;
+		border-radius: 0.4rem;
+		background: var(--clr-bg);
+		border: 0.1rem solid var(--clr-bg-secondary);
+		padding: 1.2rem;
+		color: var(--clr-text-primary);
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		z-index: 20;
+		min-width: 30rem;
+	}
+
+	.dialog-top {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 3.2rem;
+	}
+
+	.heading {
+		font-size: 2rem;
+		color: var(--clr-text-primary);
+		font-weight: 700;
+		text-align: center;
+	}
+
+	.close-btn {
+		color: var(--clr-text-secondary);
+	}
+
+	.close-btn:hover {
+		color: var(--clr-text-secondary-hover);
+	}
+
+	.dialog-content {
+		min-width: 25rem;
+		max-width: 35rem;
+		color: var(--clr-text-secondary);
+	}
+</style>
