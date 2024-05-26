7a376beec8f2cec4262de1c64c568a41d43730a9
Chad Adams <cadamsdev@outlook.com>
2024-05-26 12:27:48 -0400

    fix: redirect home route to notes route if the user has a note (#8)

--- apps/web-app/src/routes/+page.server.ts
+++ apps/web-app/src/routes/+page.server.ts
@@ -1,5 +1,15 @@
 import type { Actions } from './$types';
-import { createNote } from '$lib/server/db';
+import { createNote, getNotes } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
+
+export async function load() {
+	const notes = getNotes();
+  if (notes.length) {
+    redirect(307, `/note/${notes[0].id}`);
+  }
+
+  return {};
+}
 
 export const actions = {
 	default: async () => {
