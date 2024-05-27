154557ecb9936f74199743f2309273ffca95266f
Chad Adams <cadamsdev@outlook.com>
2024-05-27 00:36:48 -0400

    fix: issue with navigating to selected note (#12)

--- apps/web-app/src/routes/+layout.server.ts
+++ apps/web-app/src/routes/+layout.server.ts
@@ -1,8 +1,16 @@
 import { getNotes } from "$lib/server/db";
+import type { Note } from "../store";
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async () => {
+export const load: LayoutServerLoad = async ({ params }) => {
+	const notes = getNotes();
+	let selectedNote: Note | undefined = undefined;
+	if (params.id && notes.length) {
+		const id = +params.id;
+		selectedNote = notes.find((note) => note.id === id) || notes[0];
+	}
 	return {
-		notes: getNotes(),
+		notes,
+		selectedNote,
 	};
 };
