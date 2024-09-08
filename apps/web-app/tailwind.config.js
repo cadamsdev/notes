e526661e42d96d5dbf5814aa46b546de5fbe0198
cadamsdev <cadamsdev@outlook.com>
2024-09-08 18:04:31 -0400

    chore: migrated Input to tailwind

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'bg-secondary': '#211f21',
+				'bg-secondary-hover': '#454245',
+				'text-secondary': '#b3b3b3',
+			}
+		}
 	},
 	plugins: []
 };
