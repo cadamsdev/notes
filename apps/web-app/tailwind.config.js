a9ebba669caa7e33edf0d3887a91ba51c0193e27
cadamsdev <cadamsdev@outlook.com>
2024-09-08 18:11:08 -0400

    chore: converted button to tailwind

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -4,9 +4,14 @@ export default {
 	theme: {
 		extend: {
 			colors: {
+				primary: '#e78e1e',
+				'primary-hover': '#eb9f40',
+				'secondary': '#a39995',
+				'secondary-hover': '#b1a8a5',
 				'bg-secondary': '#211f21',
 				'bg-secondary-hover': '#454245',
 				'text-secondary': '#b3b3b3',
+				'btn-text': '#151415',
 			}
 		}
 	},
