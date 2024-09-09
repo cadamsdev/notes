6a622663084a0eac6aac2bfc50c42d063e876c75
cadamsdev <cadamsdev@outlook.com>
2024-09-08 21:25:13 -0400

    chore: migrate chip styles to tailwind

--- apps/web-app/src/components/Chip.svelte
+++ apps/web-app/src/components/Chip.svelte
@@ -1,86 +1,41 @@
 <script lang="ts">
-	import Icon from "@iconify/svelte";
-	import clsx from "clsx";
-	import { createEventDispatcher } from "svelte";
+	import Icon from '@iconify/svelte';
+	import clsx from 'clsx';
+	import { createEventDispatcher } from 'svelte';
 
-  export let text: string;
+	export let text: string;
 	export let color = '';
 	export let hasCloseBtn = false;
 
 	const dispatch = createEventDispatcher();
 </script>
 
-<div class="chip">
+<div
+	class="chip inline-flex items-center gap-1 py-[0.125rem] px-1 rounded-sm bg-bg-on-secondary text-tag-default-text text-xs"
+>
 	{#if color}
-		<div class={clsx({ "color": true }, { [`${color}`]: true })}></div>
+		<div
+			class={clsx(
+				{ 'w-3 h-3 rounded-[0.625rem]': true },
+				{
+					'bg-tag-red': color === 'red',
+					'bg-tag-green': color === 'green',
+					'bg-tag-blue': color === 'blue',
+					'bg-tag-purple': color === 'purple',
+					'bg-tag-yellow': color === 'yellow',
+					'bg-tag-orange': color === 'orange',
+					'bg-tag-pink': color === 'pink',
+					'bg-tag-brown': color === 'brown',
+					'bg-tag-light-gray': color === 'light-gray',
+					'bg-tag-dark-gray': color === 'dark-gray'
+				}
+			)}
+		></div>
 	{/if}
 	<div>{text}</div>
 	{#if hasCloseBtn}
-		<button on:click={() => dispatch('close')} class="close-btn">
+		<button on:click={() => dispatch('close')} class="p-1">
 			<Icon icon="fa-solid:times" width="12" height="12" />
 		</button>
 	{/if}
 </div>
-
-<style>
-  .chip {
-		display: inline-flex;
-		align-items: center;
-		gap: 0.4rem;
-		padding: 0.2rem 0.4rem;
-		font-size: 1.2rem;
-		background: var(--clr-bg-on-secondary);
-		color: var(--clr-tag-default-text);
-		border-radius: 0.2rem;
-	}
-
-	.close-btn {
-		padding: 0.4rem;
-	}
-
-	.color {
-		width: 1.2rem;
-		height: 1.2rem;
-		border-radius: 10rem;
-	}
-
-	.red {
-		background: var(--clr-tag-red);
-	}
-
-	.green {
-		background: var(--clr-tag-green);
-	}
-
-	.blue {
-		background: var(--clr-tag-blue);
-	}	
-
-	.purple {
-		background: var(--clr-tag-purple);
-	}
-
-	.yellow {
-		background: var(--clr-tag-yellow);
-	}
-
-	.orange {
-		background: var(--clr-tag-orange);
-	}
-
-	.pink {
-		background: var(--clr-tag-pink);
-	}
-
-	.brown {
-		background: var(--clr-tag-brown);
-	}
-
-	.light-gray {
-		background: var(--clr-tag-light-gray);
-	}
-
-	.dark-gray {
-		background: var(--clr-tag-dark-gray);
-	}
-</style>
