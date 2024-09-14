0e7aae6923bf917965f0a9e9c4f3d4276e3cb20c
cadamsdev <cadamsdev@outlook.com>
2024-09-14 12:47:35 -0400

    setup simple fastify rest api

---
+++ apps/api/src/index.ts
@@ -0,0 +1,20 @@
+import Fastify, { FastifyInstance } from 'fastify';
+
+const server: FastifyInstance = Fastify({
+  logger: true,
+});
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
