19e12e711e29db541a95f3e91b2cfb1f78843950
cadamsdev <cadamsdev@outlook.com>
2024-09-14 23:00:17 -0400

    wip

--- apps/web-app/src/routes/api/notes/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { getNotes } from '$lib/server/db';
-
-export const GET: RequestHandler = async () => {
-	const notes = getNotes();
-	return json(notes);
-};
