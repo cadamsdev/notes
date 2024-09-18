8e4659cef1b7d5449102ed008604ed038394cc75
cadamsdev <cadamsdev@outlook.com>
2024-09-17 22:44:29 -0400

    clean up code

--- apps/desktop-app/main.go
+++ apps/desktop-app/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "myproject",
+		Title:  "Dev Notes",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
