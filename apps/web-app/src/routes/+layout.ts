5f4a6fbabe0646a7f973c70dc41c80bc1b33d4ed
cadamsdev <cadamsdev@outlook.com>
2024-09-18 21:36:06 -0400

    fix: redirect home route to first note if there's notes

--- apps/web-app/src/routes/+layout.ts
+++ apps/web-app/src/routes/+layout.ts
@@ -1,21 +1,14 @@
 import { fetchNotes } from '../store.js';
 import * as api from '../lib/api';
-// import { redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ fetch }) => {
+export const load = async ({ url }) => {
 	const notes = await fetchNotes();
-
-  // if (notes.length) {
-	// 	redirect(307, `/note/${notes[0].id}`);
-	// }
-
 	const tagSort = await api.fetchTagSort();
 
-		// let selectedNote: Note | undefined = undefined;
-		// if (params.id && notes.length) {
-		// 	const id = +params.id;
-		// 	selectedNote = notes.find((note) => note.id === id) || notes[0];
-		// }
+	if (url.pathname === '/' && notes.length > 0) {
+		throw redirect(307, `/note/${notes[0].id}`);
+	}
 
 	return {
 		notes,
