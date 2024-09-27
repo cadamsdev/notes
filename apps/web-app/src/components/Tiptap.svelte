c1135d19d0a582c288cdafbd9fdf8a1289c919be
cadamsdev <cadamsdev@outlook.com>
2024-09-26 20:04:23 -0400

    remove buttons

--- apps/web-app/src/components/Tiptap.svelte
+++ apps/web-app/src/components/Tiptap.svelte
@@ -79,7 +79,7 @@
 	}
 
 	onMount(() => {
-		selectedNote.subscribe((note) => {
+		selectedNote.subscribe(() => {
 			initEditor();
 		});
 	});
@@ -91,32 +91,4 @@
 	});
 </script>
 
-{#if editor}
-	<button
-		on:click={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-		class:active={editor.isActive('heading', { level: 1 })}
-	>
-		H1
-	</button>
-	<button
-		on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-		class:active={editor.isActive('heading', { level: 2 })}
-	>
-		H2
-	</button>
-	<button
-		on:click={() => editor.chain().focus().setParagraph().run()}
-		class:active={editor.isActive('paragraph')}
-	>
-		P
-	</button>
-{/if}
-
 <div class="editor" bind:this={element} />
-
-<style>
-	button.active {
-		background: black;
-		color: white;
-	}
-</style>
