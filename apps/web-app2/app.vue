d3b0451a41c55977c3c316e327ae7e4eaad61c52
cadamsdev <cadamsdev@outlook.com>
2024-11-27 23:24:27 -0500

    Setup layout

--- apps/web-app2/app.vue
+++ apps/web-app2/app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="max-w-[1400px] min-h-screen mx-auto outline outline-red-300">
+    <div class="grid grid-cols-12 min-h-screen">
+      <div class="col-span-3 outline outline-green-500">
+        sidebar
+      </div>
+      <div class="col-span-6 outline outline-blue-500">
+        content
+      </div>
+      <div class="col-span-3 outline outline-yellow-500">
+        right side
+      </div>
+    </div>
   </div>
 </template>
--- apps/web-app2/nuxt.config.ts
+++ apps/web-app2/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/assets/css/base.css'],
+});
