9620c13744e3a1d61f9ee66d38194b22b0a98780
cadamsdev <cadamsdev@outlook.com>
2025-02-02 00:41:35 -0500

    fix: exit on triple enter

--- apps/web-app/lib/tiptap/extensions/CodeBlock.ts
+++ apps/web-app/lib/tiptap/extensions/CodeBlock.ts
@@ -69,6 +69,37 @@ const CodeBlock = Node.create({
     ];
   },
 
+  addKeyboardShortcuts() {
+    return {
+      Enter: ({ editor }) => {
+        const { state } = editor;
+        const { selection } = state;
+        const { $from, empty } = selection;
+
+        if (!empty || $from.parent.type !== this.type) {
+          return false;
+        }
+
+        const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
+        const endsWithDoubleNewline = $from.parent.textContent.endsWith('\n\n');
+
+        if (!isAtEnd || !endsWithDoubleNewline) {
+          return false;
+        }
+
+        return editor
+          .chain()
+          .command(({ tr }) => {
+            tr.delete($from.pos - 2, $from.pos);
+
+            return true;
+          })
+          .exitCode()
+          .run();
+      },
+    };
+  },
+
   addNodeView() {
     return VueNodeViewRenderer(Component);
   },
