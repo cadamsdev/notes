eded78be437406e030936b88938ccf19fe20f214
cadamsdev <cadamsdev@outlook.com>
2025-03-10 20:11:53 -0400

    fix setting numbered list and ordered list

--- apps/web-app/lib/tiptap/extensions/slash-commands/suggestions.ts
+++ apps/web-app/lib/tiptap/extensions/slash-commands/suggestions.ts
@@ -1,174 +1,175 @@
-import { VueRenderer } from '@tiptap/vue-3';
+import { Editor, VueRenderer, type Range } from '@tiptap/vue-3';
 import tippy from 'tippy.js';
 import CommandsList from './CommandsList.vue';
 
+interface CommandItem {
+  title: string;
+  icon: string;
+  command: ({ editor, range }: { editor: Editor; range: Range }) => void;
+}
+
+const commands: CommandItem[] = [
+  {
+    title: 'Heading 1',
+    icon: 'codex:h1',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 1 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 2',
+    icon: 'codex:h2',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 2 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 3',
+    icon: 'codex:h3',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 3 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 4',
+    icon: 'codex:h4',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 4 })
+        .run();
+    },
+  },
+  {
+    title: 'Heading 5',
+    icon: 'codex:h5',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('heading', { level: 5 })
+        .run();
+    },
+  },
+  {
+    title: 'Bulleted List',
+    icon: 'codex:list-bulleted',
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleBulletList().run();
+    },
+  },
+  {
+    title: 'Numbered List',
+    icon: 'codex:list-numbered',
+    command: ({ editor, range }) => {
+      editor.chain().focus().deleteRange(range).toggleOrderedList().run();
+    },
+  },
+  {
+    title: 'Code',
+    icon: 'codex:curly-brackets',
+    command: ({ editor, range }) => {
+      editor
+        .chain()
+        .focus()
+        .deleteRange(range)
+        .setNode('codeBlock', { language: 'text' })
+        .run();
+    },
+  },
+];
+
+function items({ query }: { query: string }): CommandItem[] {
+  return commands
+    .filter((item) => item.title.toLowerCase().startsWith(query.toLowerCase()))
+    .slice(0, 10);
+}
+
+function render() {
+  let component;
+  let popup;
+
+  return {
+    onStart: (props) => {
+      function onClose() {
+        popup[0].hide();
+        props.editor
+          .chain()
+          .deleteRange({
+            from: props.editor.state.selection.from - 1,
+            to: props.editor.state.selection.from,
+          })
+          .focus()
+          .run();
+      }
+
+      console.log('onStart');
+      component = new VueRenderer(CommandsList, {
+        props: { ...props, onClose },
+        editor: props.editor,
+      });
+
+      if (!props.clientRect) {
+        return;
+      }
+
+      popup = tippy('body', {
+        getReferenceClientRect: props.clientRect,
+        appendTo: () => document.body,
+        content: component.element,
+        showOnCreate: true,
+        interactive: true,
+        trigger: 'manual',
+        placement: 'bottom-start',
+      });
+    },
+
+    onUpdate(props) {
+      component.updateProps(props);
+
+      if (!props.clientRect) {
+        return;
+      }
+
+      popup[0].setProps({
+        getReferenceClientRect: props.clientRect,
+      });
+    },
+
+    onKeyDown(props) {
+      // if (props.event.key === 'Escape') {
+      //   popup[0].hide();
+      //   return true;
+      // }
+      // return component.ref?.onKeyDown(props);
+    },
+
+    onExit() {
+      popup[0].destroy();
+      component.destroy();
+    },
+  };
+}
+
 export default {
-  items: ({ query }) => {
-    return [
-      {
-        title: 'Heading 1',
-        icon: 'codex:h1',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('heading', { level: 1 })
-            .run();
-        },
-      },
-      {
-        title: 'Heading 2',
-        icon: 'codex:h2',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('heading', { level: 2 })
-            .run();
-        },
-      },
-      {
-        title: 'Heading 3',
-        icon: 'codex:h3',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('heading', { level: 3 })
-            .run();
-        },
-      },
-      {
-        title: 'Heading 4',
-        icon: 'codex:h4',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('heading', { level: 4 })
-            .run();
-        },
-      },
-      {
-        title: 'Heading 5',
-        icon: 'codex:h5',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('heading', { level: 5 })
-            .run();
-        },
-      },
-      {
-        title: 'Bulleted List',
-        icon: 'codex:list-bulleted',
-        command: ({ editor, range }) => {
-          editor.chain().focus().deleteRange(range).setNode('bulletList').run();
-        },
-      },
-      {
-        title: 'Numbered List',
-        icon: 'codex:list-numbered',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('orderedList')
-            .run();
-        },
-      },
-      {
-        title: 'Code',
-        icon: 'codex:curly-brackets',
-        command: ({ editor, range }) => {
-          editor
-            .chain()
-            .focus()
-            .deleteRange(range)
-            .setNode('codeBlock', { language: 'text' })
-            .run();
-        },
-      },
-    ]
-      .filter((item) =>
-        item.title.toLowerCase().startsWith(query.toLowerCase())
-      )
-      .slice(0, 10);
-  },
-
-  render: () => {
-    let component;
-    let popup;
-
-    return {
-      onStart: (props) => {
-
-        function onClose() {
-          popup[0].hide();
-          props.editor
-            .chain()
-            .deleteRange({
-              from: props.editor.state.selection.from - 1,
-              to: props.editor.state.selection.from,
-            })
-            .focus()
-            .run();
-        }
-
-        console.log('onStart')
-        component = new VueRenderer(CommandsList, {
-          props: {...props, onClose},
-          editor: props.editor,
-        });
-
-        if (!props.clientRect) {
-          return;
-        }
-
-        popup = tippy('body', {
-          getReferenceClientRect: props.clientRect,
-          appendTo: () => document.body,
-          content: component.element,
-          showOnCreate: true,
-          interactive: true,
-          trigger: 'manual',
-          placement: 'bottom-start',
-        });
-      },
-
-      onUpdate(props) {
-        component.updateProps(props);
-
-        if (!props.clientRect) {
-          return;
-        }
-
-        popup[0].setProps({
-          getReferenceClientRect: props.clientRect,
-        });
-      },
-
-      onKeyDown(props) {
-        // if (props.event.key === 'Escape') {
-        //   popup[0].hide();
-
-        //   return true;
-        // }
-
-        // return component.ref?.onKeyDown(props);
-      },
-
-      onExit() {
-        popup[0].destroy();
-        component.destroy();
-      },
-    };
-  },
+  items,
+  render,
 };
