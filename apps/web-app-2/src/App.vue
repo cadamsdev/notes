2ed6cbe8af0622292f39d85558c17e32db3ce9ed
cadamsdev <cadamsdev@outlook.com>
2025-09-13 11:55:33 -0400

    setup tailwind

--- apps/web-app-2/src/App.vue
+++ apps/web-app-2/src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import './styles/global.css';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-3xl font-bold underline text-green-500">
+    Hello world!
+  </h1>
 </template>
-
-<style scoped></style>
