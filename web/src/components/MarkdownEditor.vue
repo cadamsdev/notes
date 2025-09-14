c2cdf2bc1f627a3ea698a851501f06e55a486ee4
cadamsdev <cadamsdev@outlook.com>
2025-09-14 16:21:11 -0400

    wip

--- web/src/components/MarkdownEditor.vue
+++ web/src/components/MarkdownEditor.vue
@@ -10,14 +10,27 @@ import { ref } from 'vue';
 const editor = ref<HTMLElement | null>(null);
 
 function handleKeyDown(event: KeyboardEvent) {
+  const target = event.target as HTMLElement;
+  console.log('Key down event:', event.key, 'on target:', target);
+
   if (event.key === 'Enter') {
     console.log('Enter key pressed');
     createParagraph();
+  } else if (event.key === 'Backspace' && target !== editor.value) {
+    console.log('Backspace key pressed');
+
+    const target = event.target as HTMLElement;
+    if (target && target.textContent.length === 0) {
+      target.remove();
+      event.preventDefault();
+      editor.value?.focus();
+    }
   }
 }
 
 function createParagraph() {
   const paragraph = document.createElement('p');
+  // paragraph.addEventListener('keydown', onChildKeyDown);
   paragraph.textContent = '';
   paragraph.contentEditable = "true";
   editor.value?.appendChild(paragraph);
