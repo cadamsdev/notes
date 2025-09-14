9119455d2d85053bf45e97e60e10d306378a2d78
cadamsdev <cadamsdev@outlook.com>
2025-09-14 16:04:41 -0400

    wip

--- web/src/components/MarkdownEditor.vue
+++ web/src/components/MarkdownEditor.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    Hello world
+  <div ref="editor" tabindex="0" @keydown="handleKeyDown">
+    <span id="placeholder">Any thoughts...</span>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const editor = ref<HTMLElement | null>(null);
+
+function handleKeyDown(event: KeyboardEvent) {
+  if (event.key === 'Enter') {
+    console.log('Enter key pressed');
+    createParagraph();
+  }
+}
+
+function createParagraph() {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = '';
+  paragraph.contentEditable = "true";
+  editor.value?.appendChild(paragraph);
+  paragraph.focus();
+}
 
 </script>
